88ab1f634e58ece58b6b8e8c20845182b91e2caa
itamim-coder <itamim12202@gmail.com>
2025-08-14 20:40:54 +0600

    [INIT] Codebase for air

---
+++ src/app/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  default_password: process.env.DEFAULT_PASS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  reset_pass_secret: process.env.RESET_PASS_TOKEN,
+  reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN,
+
+  reset_pass_link: process.env.RESET_PASS_LINK,
+  emailSender: {
+    email: process.env.EMAIL,
+    app_pass: process.env.APP_PASS,
+  },
+};
